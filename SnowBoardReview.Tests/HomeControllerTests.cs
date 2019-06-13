65f52885c93c7ea6a954285520c85ccdea1a94ef
ladejesus <luisito.dejesus@gmail.com>
2019-06-13 11:18:30 -0400

    added test, views and controllers folders

---
+++ SnowBoardReview.Tests/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using SnowBoardReview.Controllers;
+using Xunit;
+
+namespace SnowBoardReview.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_Hello_World()
+        {
+            var underTest = new HomeController();
+
+            var result = underTest.Index();
+
+            Assert.Equal("Hello World!", result);
+        }
+    }
+}
