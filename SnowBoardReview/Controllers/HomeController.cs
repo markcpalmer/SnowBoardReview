2fec5c2cf57257faba4dc8d59bcdfd97ef3f8449
Mark Palmer <mark.palmer@itsalllive.com>
2019-06-14 14:07:21 -0400

    made the view produce html

--- SnowBoardReview/Controllers/HomeController.cs
+++ SnowBoardReview/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SnowBoardReview.Models;
 
 namespace SnowBoardReview.Controllers
 {
     public class HomeController: Controller
     {
+        //public Review(string productDescription, string productName, string categoryName, int reviewID, string userReview, string productImage)
         public ViewResult Index()
-        {         
-            return View();
+        {
+            Review userReview = new Review("a snowBoard", "snowboard 1", "freestyle", 100, "great board for parks", "fake img");
+            return View(userReview);
         }
     }
 }
