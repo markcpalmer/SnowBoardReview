5f72afff34ca965de24857aa79f27d085eda0daa
ladejesus <luisito.dejesus@gmail.com>
2019-06-19 12:01:42 -0400

    added context class and database stuff

---
+++ SnowBoardReview/SnowBoardContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SnowBoardReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnowBoardReview
+{
+    public class SnowBoardContext : DbContext
+    {
+        public DbSet<Snowboard> Snowboards { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server = (localdb)\\mssqllocaldb; Database = SnowBoardTesting; Trusted_Connection = True";
+
+            optionsBuilder.UseSqlServer(connectionString)
+                        .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+
+        }
+    }
+}
