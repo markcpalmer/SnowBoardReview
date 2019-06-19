a0b7840f648783c2c37e5ce350c487686009ca1a
ladejesus <luisito.dejesus@gmail.com>
2019-06-19 11:40:34 -0400

    added Brand class and nuget packages

---
+++ SnowBoardReview/Models/Snowboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnowBoardReview.Models
+{
+    public class Snowboard
+    {
+        public int ID { get; set; }
+        public string ModelDescription { get; set; }
+        public string ModelName { get; set; }
+        public string BrandName { get; set; }
+        //public int ReviewID { get; set; }
+        //public string UserReview { get; set; }
+        public string ProductImage { get; set; }
+
+        public Snowboard(int id, string modelDescription, string modelName, string brandName, string productImage)
+        {
+            ID = id;
+            ModelDescription = modelDescription;
+            ModelName = modelName;
+            BrandName = brandName;
+            ProductImage = productImage;
+        }
+    }
+}
