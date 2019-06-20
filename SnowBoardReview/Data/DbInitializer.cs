781c0788bac19d36fbd30790cbe7aa3d9a4b90d2
Bridget Klingler <bridget.klingler@gmail.com>
2019-06-19 23:20:01 -0400

    Tried to add initializers, models not updating

--- SnowBoardReview/Data/DbInitializer.cs
+++ SnowBoardReview/Data/DbInitializer.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SnowBoardReview.Models;
 
 namespace SnowBoardReview.Data
 {
     public class DbInitializer
     {
+        public static void Initialize(SnowBoardContext context)
+        {
+            var rev = new Review("gnarly", "TooCool", "Board", 1, "That's cool", "img123");
+            context.Add<Review>(rev);
+        }
+
+
     }
 }
